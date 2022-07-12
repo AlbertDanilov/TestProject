a2f9b1d31e5947dcdafcfaab05c860e86b976ce7
Albert Danilov <albert95danilov@gmail.com>
2022-07-12 15:28:39 +0300

    кортеж

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -10,6 +10,23 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            //кортеж
+
+            int n1 = 5;
+            int n2 = 7;
+
+            Console.WriteLine($"n1 = {n1}, n2 = {n2}");
+            (n1, n2) = (n2, n1);
+            Console.WriteLine($"n1 = {n1}, n2 = {n2}");
+
+            //кортеж
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
