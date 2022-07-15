2527fac6a4ad3ba59329ef2c6ebff8614485d736
Albert Danilov <albert95danilov@gmail.com>
2022-07-15 07:50:56 +0300

    bin compare

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Linq;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,25 +16,25 @@ namespace TestProject
         {
             //кортеж и операции с ним
 
-            int n1 = 5;
-            int n2 = 7;
+            //int n1 = 5;
+            //int n2 = 7;
 
             //Console.WriteLine($"n1 = {n1}, n2 = {n2}");
-            (n1, n2) = (n2, n1);
+            //(n1, n2) = (n2, n1);
             //Console.WriteLine($"n1 = {n1}, n2 = {n2}");
 
-            List<int> _list = new List<int>(new int[] { 1, 2, 3 });
-            int[] _array = { 1, 2, 3, 4, 5 };
+            //List<int> _list = new List<int>(new int[] { 1, 2, 3 });
+            //int[] _array = { 1, 2, 3, 4, 5 };
 
-            (int, int) result = Helpers.OtherHelpers.Calc_Sum_Count(_list);
+            //(int, int) result = Helpers.OtherHelpers.Calc_Sum_Count(_list);
             //Console.WriteLine($"sum = {result.Item1}, count = {result.Item2}");
 
             //кортеж и операции с ним
 
-            int a, b, c;
-            a = b = c = 34 * 2 / 4; // 17
+            //int a, b, c;
+            //a = b = c = 34 * 2 / 4; // 17
 
-            int x = n1 >= n2 ? 1 : 0;
+            //int x = n1 >= n2 ? 1 : 0;
             //Console.WriteLine(x);
 
             //for (int i = 1;i < 5; i++)
@@ -47,7 +49,7 @@ namespace TestProject
 
             //Console.WriteLine(_array[^1]);
 
-            int fc = Helpers.OtherHelpers.Factorial(10);
+            //int fc = Helpers.OtherHelpers.Factorial(10);
             //Console.WriteLine(fc);
 
             //switch (a)
@@ -60,38 +62,38 @@ namespace TestProject
             //Console.WriteLine(Helpers.OtherHelpers.types.first);
             //Console.WriteLine(Helpers.OtherHelpers.types.first.ToString());
 
-            int? t1 = null;
+            //int? t1 = null;
             //Console.WriteLine(t1);
 
-            Person _person = new Person("Albert", "Danilov");
+            //Person _person = new Person("Albert", "Danilov");
 
             //_person.SecondName = "";
 
-            Object _obj1 = new Object();
-            Object _obj2 = new Object();
+            //Object _obj1 = new Object();
+            //Object _obj2 = new Object();
 
             //Console.WriteLine(_obj1.ToString());
             //Console.WriteLine(_obj1.GetHashCode());
             //Console.WriteLine(_obj2.GetHashCode());
             //Console.WriteLine(_obj2.GetType());
 
-            bool eq = _obj1.Equals(_obj2) ? true : false;
+            //bool eq = _obj1.Equals(_obj2) ? true : false;
 
             //Console.WriteLine(eq);
 
-            Lazy<Person> _person2 = new Lazy<Person>();
-            _person2.Value.Name = "Albert";
+            //Lazy<Person> _person2 = new Lazy<Person>();
+            //_person2.Value.Name = "Albert";
 
             //Console.WriteLine(_person2.Value.Name);
 
 
             //Hashtable tbl = new Hashtable();
 
-            int[] _array2 = { 5, 7, 4, 2, 6, 9, 7, 65, 4, 3, 2, 2, 3 };
+            //int[] _array2 = { 5, 7, 4, 2, 6, 9, 7, 65, 4, 3, 2, 2, 3 };
 
-            foreach (int item in _array2) Console.Write($"{item} ");
-            Console.WriteLine();
-            Console.WriteLine();
+            //foreach (int item in _array2) Console.Write($"{item} ");
+            //Console.WriteLine();
+            //Console.WriteLine();
 
             //Helpers.Sorting.Bubble_Sort(ref _array2);
 
@@ -111,8 +113,26 @@ namespace TestProject
             //Console.WriteLine(rez2);
 
             //foreach (int item in _array2) Console.Write($"{item} "); 
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            int[] ar1 = { 1, 2, 3, 4, 5 };
+            int[] ar2 = { 1, 2, 3, 4, 5 };
+
+            byte[] f1 = File.ReadAllBytes("D:\\#БазыАптеки\\Электронный чек1.pdf");
+            byte[] f2 = File.ReadAllBytes("D:\\#БазыАптеки\\Электронный чек1.pdf");
+
+            bool IsEqual = false;
+
+            Console.WriteLine($"Before IsEqual = {IsEqual}");
+
+            //IsEqual = System.Data.Linq.Binary.Equals(p1, p2);
+            IsEqual = System.Data.Linq.Binary.Equals(ar1, ar2);
+
+            Console.WriteLine($"After IsEqual = {IsEqual}");
 
             Console.ReadLine();
         }
